8a4f7de02b4ad9fef985b10827a3b5946f680daf
arlan-trislis <arlanariandi@gmail.com>
2020-02-04 17:18:40 +0700

    upload logika5.dart

--- novice/01-02/kasus/algoritma/part1/logika5.dart
+++ novice/01-02/kasus/algoritma/part1/logika5.dart
@@ -1,1 +1,20 @@
+// Same number in both arrays
 
+main() {
+  var a = ['1', '2', '3'];
+  var b = ['1', '2', '3'];
+
+  var i = 0;
+  var j = 0;
+
+  while (i == (a.length - 1) && j == (b.length - 1)) {
+    if (a[i] == b[j]) {
+      print(i = i + 1);
+    } else {
+      if (a[i] == b[j]) {
+        print(j = j + 1);
+      }
+    }
+  }
+  print(a);
+}
