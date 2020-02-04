876e685b881e34665aff7310cb412589ac7de4ac
arlan-trislis <arlanariandi@gmail.com>
2020-02-04 16:33:40 +0700

    pembaharuan ada pada folder 01-02 kasus file logika4.dart

--- novice/01-02/kasus/algoritma/part1/logika4.dart
+++ novice/01-02/kasus/algoritma/part1/logika4.dart
@@ -1,3 +1,22 @@
-// Backward Insertion Sort
+// Same number in both arrays
 
-main() {}
+main() {
+  var a = [1, 2, 3, 4, 5];
+  var b = [1, 2, 3, 4, 5];
+
+  var i = 0;
+  var j = 0;
+
+  while (i == (a.length - 1) && j == (b.length - 1)) {
+    if (a[i] < b[j]) {
+      print(i = i + 1);
+    } else {
+      if (a[i] > b[j]) {
+        print(j = j + 1);
+      } else {
+        true;
+      }
+    }
+  }
+  print(false);
+}
